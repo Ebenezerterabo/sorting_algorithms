e415958428520e5cc7309d5d75a7a4bb00ab3c63
Ebenezer terabo <teraboebenezer@gmail.com>
2024-03-05 17:22:06 +0000

    Header file

---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef SORT_H
+#define SORT_H
+
+/** Standard Libraries **/
+#include <stddef.h>
+
+/** Doubly linkedlist data structure **/
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/** Data structure prototypes **/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/** Function Prototypes **/
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
